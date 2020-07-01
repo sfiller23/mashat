0332f1f000751ceb50e293cea0ed7423b991c675
sfiller23 <sf27lp@gmail.com>
2020-07-01 20:16:19 +0300

    user model+migrations

--- app.js
+++ app.js
@@ -3,6 +3,28 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+/*const { Sequelize } = require('sequelize');
+
+
+const database = 'mashat_db';
+const username = 'postgres';
+const password = 'mteburhx';
+
+const sequelize = new Sequelize(database, username, password, {
+  host: 'localhost',
+  dialect: 'postgres'
+});*/
+//testing: the run function is for: 'You can only use await inside an async function, not at the top level. There's a proposal to support top-level await, but that is not currently supported in JS'
+async function run() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+//for testing the connection:
+//run().catch(error => console.log(error.stack));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');

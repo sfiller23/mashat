bec8c6f7d464704e46c57ac4c2baa879a06f1751
sfiller23 <sf27lp@gmail.com>
2020-07-02 21:06:22 +0300

    delete fs req in config

--- config/config.js
+++ config/config.js
@@ -1,13 +1,3 @@
-const fs = require('fs');
-const dotenv = require('dotenv');
-dotenv.config();
-
-module.exports = {
-  endpoint: process.env.API_URL,
-  masterKey: process.env.API_KEY,
-  env: process.env.NODE_ENV
-};
-
 module.exports = {
   "development": {
     "username": "postgres",
